2c7e10b826b7e4f317b2d3aa5515dbdace71773a
Valeria Chubarova <chubarova.valeria99@gmail.com>
2024-09-10 16:48:29 +0300

    make build

---
+++ index.aa5f655d.js
@@ -0,0 +1,2 @@
+var e=document.querySelector("table");["thead","tbody","tfoot"].forEach(function(o){e.querySelectorAll("".concat(o," tr")).forEach(function(e){var o=Array.from(e.querySelectorAll("td, th")),r=o[1].cloneNode(!0);e.insertBefore(r,o[o.length-1].previousSibling)})});
+//# sourceMappingURL=index.aa5f655d.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,20 @@
+"use strict";
+const table = document.querySelector("table");
+function cloneSecondColumn() {
+    const sections = [
+        "thead",
+        "tbody",
+        "tfoot"
+    ];
+    sections.forEach((section)=>{
+        const rows = table.querySelectorAll(`${section} tr`);
+        rows.forEach((row)=>{
+            const cells = Array.from(row.querySelectorAll("td, th"));
+            const secondCell = cells[1].cloneNode(true);
+            row.insertBefore(secondCell, cells[cells.length - 1].previousSibling);
+        });
+    });
+}
+cloneSecondColumn();
+
+//# sourceMappingURL=index.f75de5e1.js.map
